5e9f4d1dc92284508ad1ca0ae72b7e2dbcd306af
kevinozkaraca <kevin.ozkaraca@gmail.com>
2022-01-17 14:04:42 +0100

    mise a jour

---
+++ main.py
@@ -0,0 +1,101 @@
+from rich import print
+from rich.table import Table
+from rich.console import Console
+from rich.progress import track
+import time
+
+presentation = '''
+oooooooooo.   o8o                                                                                
+`888'   `Y8b  `"'                                                                                
+ 888     888 oooo   .ooooo.  ooo. .oo.   oooo    ooo  .ooooo.  ooo. .oo.   oooo  oooo   .ooooo.  
+ 888oooo888' `888  d88' `88b `888P"Y88b   `88.  .8'  d88' `88b `888P"Y88b  `888  `888  d88' `88b 
+ 888    `88b  888  888ooo888  888   888    `88..8'   888ooo888  888   888   888   888  888ooo888 
+ 888    .88P  888  888    .o  888   888     `888'    888    .o  888   888   888   888  888    .o 
+o888bood8P'  o888o `Y8bod8P' o888o o888o     `8'     `Y8bod8P' o888o o888o  `V88V"V8P' `Y8bod8P' '''
+
+# Pour afficher un message de ce genre chercher : acsii art générator !
+'''
+----------- Installation de la librairie rich
+ 
+    # Dans le terminal
+>   pip install rich
+    # Affiche les fonctionnalités de la librairie
+>   python -m rich
+
+----------- Importation des fonctionnalités
+ 
+    # Importe les fonctionnalités
+from rich import print
+from rich.table import Table
+from rich.console import Console
+from rich.progress import track
+import time
+
+----------- Fonctionnalité du print
+
+    # ecrire en gras
+print("[bold]Ce que je veux)"
+    # écrire rouge sur blanc
+print("[red on white]plein de cadeau")
+    # écrire une partie seulement rouge sur blanc
+print("[red on white]plein[/] de cadeau")
+
+----------- Faire un tableau
+
+table = Table(title="Tableau de test")
+
+table.add_column("Colonne1", style="cyan")
+table.add_column("Colonne2", style="magenta")
+table.add_column("Colonne3", style="white")
+table.add_column("Colonne4", style="white")
+
+table.add_row("info1", "info2", "info3", "info4")
+table.add_row("info1", "info2", "info3", "info4")
+table.add_row("info1", "info2", "info3", "info4")
+
+table.add_row("info1", "info2", "info3", "info4")
+
+console = Console()
+console.print(table)
+
+----------- Faire une barre de progression
+
+for i in track(range(100), description= "Chargement en cours"):
+    time.sleep(0.1)
+'''
+
+
+# Les exemples du print
+print(presentation)
+time.sleep(1)
+print("[bold]Ce que je veux")
+print("[red on white]plein de cadeau")
+print("[red on white]plein[/] de cadeau")
+
+time.sleep(1)
+# Exemple de tableau
+
+table = Table(title="Tableau de test")
+
+table.add_column("Colonne1", style="cyan")
+table.add_column("Colonne2", style="magenta")
+table.add_column("Colonne3", style="yellow")
+table.add_column("Colonne4", style="green")
+
+table.add_row("info1", "info2", "info3", "info4")
+table.add_row("info1", "info2", "info3", "info4")
+table.add_row("info1", "info2", "info3", "info4")
+
+table.add_row("info1", "info2", "info3", "info4")
+
+console = Console()
+console.print(table)
+
+time.sleep(1)
+# Exemple de barre de progression
+
+for i in track(range(100), description= "Chargement en cours"):
+    time.sleep(0.1)
+
+time.sleep(1)
+print("Fin du programme")
---
+++ readme.md
@@ -0,0 +1,30 @@
+### Fonctionnalités de base pour la librairie rich
+
+<p align="center">La librairie rich est une librairie qui permet de personnaliser son terminal<p>
+<br/>
+<p align="center">
+  <img align="center" width="80%" alt="commande python -m rich" src="image1.png"/>
+</p>
+<br/>
+<p align="center">Voici un exemple de ce que l'on peut réaliser<p>
+<br/>
+<p align="center">
+  <img align="center" width="80%" alt="exemple de présentation" src="image2.png"/>
+</p>
+<br/>
+
+### Mes reseaux sociaux : 
+
+<p align="center">
+  <br/>
+    <div class="socialIcons">
+            <a href="https://github.com/kevinozkaraca" target="_blank"><img width="5%" src="iconGithub.png" alt="Icone Github de Kevin Özkaraca" aria-label="Accedez à mes dépots Github" title="Accedez à mes dépots Github"></a>
+            <a href="https://www.facebook.com/kevinozkaraca" target="_blank"><img width="5%" src="iconFacebook.png" alt="Icone Facebook de Kevin Özkaraca" aria-label="Accedez à mon compte Facebook" title="Accedez à mon compte Facebook" ></a>
+             <a href="https://pinterest.com/kevinozkaraca" target="_blank"><img width="5%" src="iconPinterest.png" alt="Icone Pinterest de Kevin Özkaraca" aria-label="Accedez à mon compte Pinterest" title="Accedez à mon compte Pinterest"></a>
+            <a href="https://twitter.com/kevinozkaraca" target="_blank"><img width="5%" src="iconTwitter.png" alt="Icone Twitter de Kevin Özkaraca" aria-label="Accedez à mon compte Twitter" title="Accedez à mon compte Twitter"></a>
+            <a href="https://instagram.com/kevinozkaraca" target="_blank"><img width="5%" src="iconInstagram.png" alt="Icone Instagram de Kevin Özkaraca" aria-label="Accedez à mon compte Twitter" title="Accedez à mon compte Twitter"></a>
+            <a href="https://www.linkedin.com/in/kevin-%C3%B6zkaraca-66a256209/" target="_blank"><img width="5%" src="iconLinkedin.png" alt="Icone Linkedin de Kevin Özkaraca" aria-label="Accedez à mon  compte Linkedin" title="Accedez à mon  compte Linkedin"></a>
+            <a href="https://www.youtube.com/channel/UCgrJrS7eEZ-HpdyA6YoXRmw" target="_blank"><img width="5%" src="iconYoutube.png" alt="Icone Youtube de Kevin Özkaraca" aria-label="Accedez à ma chaine Youtube" title="Accedez à ma chaine Youtube"></a>
+            <a href="mailto:kevin.ozkaraca@gmail.com" target="_blank"><img width="5%" src="iconGmail.png" alt="Icone Gmail de Kevin Özkaraca" aria-label="Envoyez moi un mail sur mon Gmail" title="Envoyez moi un mail sur mon Gmail" ></a>
+    </div>  
+</p>
